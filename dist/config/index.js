fb5eb4807e6b3804b268e9841ad221aead695e38
jubayer1418 <skjubayer1418@gmail.com>
2024-03-29 21:22:04 +0600

    done

--- dist/config/index.js
+++ dist/config/index.js
@@ -8,6 +8,7 @@ const path_1 = __importDefault(require("path"));
 dotenv_1.default.config({ path: path_1.default.join(process.cwd(), ".env") });
 exports.default = {
     env: process.env.NODE_ENV,
+    database: process.env.DATABASE_URL,
     port: process.env.PORT,
     jwt: {
         jwt_secret: process.env.JWT_SECRET,
--- dist/server.js
+++ dist/server.js
@@ -14,11 +14,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
 const config_1 = __importDefault(require("./config"));
+const prisma_1 = __importDefault(require("./shared/prisma"));
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
-        const server = app_1.default.listen(config_1.default.port, () => {
-            console.log(`server listening on port ${config_1.default.port}`);
-        });
+        try {
+            yield prisma_1.default.$connect();
+            app_1.default.listen(config_1.default.port, () => {
+                console.log(`Example app listening on port ${config_1.default.port}`);
+            });
+        }
+        catch (error) {
+            yield prisma_1.default.$disconnect();
+        }
     });
 }
 main();
