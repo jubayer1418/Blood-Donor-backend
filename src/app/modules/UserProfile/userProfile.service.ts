1faeb7343a410f67d07fb93cb67c7353d0c49a00
jubayer1418 <skjubayer1418@gmail.com>
2024-03-29 16:27:27 +0600

    userProfile done

--- src/app/modules/UserProfile/userProfile.service.ts
+++ src/app/modules/UserProfile/userProfile.service.ts
@@ -1,7 +1,10 @@
 import prisma from "../../../shared/prisma";
 
 const getFromDb = async () => {
-  const result = await prisma.user.findMany({
+  const result = await prisma.user.findUniqueOrThrow({
+    where: {
+      id: "8b9c2c10-bf61-411d-8865-78106179e2bf",
+    },
     include: {
       userProfile: true,
     },
@@ -9,12 +12,16 @@ const getFromDb = async () => {
 
   return result;
 };
-const updateFromDb = async (id: string, payload: any) => {
-  console.log(id, payload);
-  await prisma.request.findUniqueOrThrow({ where: { id } });
-  const result = await prisma.request.update({
+const updateFromDb = async (
+  id = "1ad1d09e-9ca0-4ef1-a2c8-609b516cdd14",
+  payload: any
+) => {
+  console.log(payload);
+  await prisma.userProfile.findUniqueOrThrow({ where: { id } });
+  const result = await prisma.userProfile.update({
     where: { id },
-    data: { requestStatus: payload.status },
+
+    data: payload,
   });
   return result;
 };
