efb221d2a45fcd335a8efa4749a5b1cbf8de4d83
jubayer1418 <skjubayer1418@gmail.com>
2024-03-29 21:11:26 +0600

    done

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,13 @@ import app from "./app";
 import config from "./config";
 
 async function main() {
-  const server: Server = app.listen(config.port, () => {
-    console.log(`server listening on port ${config.port}`);
-  });
+  try {
+    const server: Server = app.listen(config.port, () => {
+      console.log(`server listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 main();
