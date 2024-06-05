5693d3a8d1d6c7485707166567a3380b8b4ca6d8
jubayer1418 <skjubayer1418@gmail.com>
2024-06-05 20:30:45 +0600

    done

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import prisma from "./shared/prisma";
 async function main() {
   try {
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
+      console.log(`Example app listening on port: ${config.port}`);
     });
   } catch (error) {
     await prisma.$disconnect();
